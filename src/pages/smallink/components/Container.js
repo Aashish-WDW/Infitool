04f15bfd50aad54ab33a5f0ca3761e5406de5662
Your Name <you@example.com>
2023-04-15 22:32:04 +0530

    tests

--- src/pages/smallink/components/Container.js
+++ src/pages/smallink/components/Container.js
@@ -1,16 +1,7 @@
 import React from 'react';
 
-const StyledDiv = styled.div`
-  display: flex;
-  height: 100vh;
-  text-align: center;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-`;
-
 const Container = ({ children }) => {
-    return <StyledDiv>{children}</StyledDiv>;
+  return { children };
 };
 
 export default Container;
