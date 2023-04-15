5dc44351f5c957edd1edc31f0c0d433a64d7bbf1
Your Name <you@example.com>
2023-04-15 22:11:28 +0530

    tests

--- src/pages/Nav/Footer.js
+++ src/pages/Nav/Footer.js
@@ -1,33 +1,34 @@
 import React from 'react';
 import styles from '../../styles/navbar.module.css';
+import Link from 'next/link'
 
 const Footer = () => {
   return (
     <div className="container-fluid" style={{ marginTop: '40px' }}>
       <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 border-top">
         <div className="col-md-4 d-flex align-items-center">
-          <a
+          <Link
             href="/"
             className="navbar-brand mb-3 me-2 mb-md-0 text-muted text-decoration-none lh-1"
           >
             INFIT
             <i className={`fa-solid fa-infinity ${styles['lol']}`}></i>
             L
-          </a>
+          </Link>
           <span className="mb-3 mb-md-0 text-muted">&copy; 2022 Company, Inc</span>
         </div>
 
         <ul className="nav col-md-4 justify-content-end list-unstyled d-flex">
-          <a href="/Terms" style={{ textDecoration: 'none', paddingRight: '10px' }}>
+          <Link href="/Terms" style={{ textDecoration: 'none', paddingRight: '10px' }}>
             <li style={{ color: '#6c757d!important' }}>Terms of Service</li>
-          </a>
-          <a href="/Privacy" style={{ textDecoration: 'none' }}>
+          </Link>
+          <Link href="/Privacy" style={{ textDecoration: 'none' }}>
             <li style={{ color: '#6c757d!important' }}>Privacy Policy</li>
-          </a>
+          </Link>
           <li className="ms-3">
-            <a className="text-muted2" href="https://discord.com/channels/1074050985804169246">
+            <Link className="text-muted2" href="https://discord.com/channels/1074050985804169246">
               <i className="fa-brands fa-discord"></i>
-            </a>
+            </Link>
           </li>
         </ul>
       </footer>
